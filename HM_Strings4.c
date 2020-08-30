3778bc708b5d71865ae667d83f1578d6273d196d
Kuntal Ojha <67447627+Kuntal-ojha@users.noreply.github.com>
2020-08-30 09:43:49 +0530

    Add files via upload

---
+++ HM_Strings4.c
@@ -0,0 +1,25 @@
+//
+// Created by KUNTAL OJHA RAJ on 8/29/2020.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
+    char *names[6];
+    char n[50];
+    int len,i;
+    char *p;
+    for (i=0;i<=5;i++)
+    {
+        printf("Enter Name:\n");
+        scanf("%s",n);
+        len=strlen(n);
+        p=(char*)malloc(len+1);
+        strcpy(p,n);
+        names[i]=p;
+    }
+    for(i=0;i<=5;i++)
+        printf("%s\n",names[i]);
+    return 0;
+}

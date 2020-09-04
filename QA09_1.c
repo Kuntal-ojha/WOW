f706b771bb6d03fba484774a2f04f69e7d898ea8
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-09-05 00:48:38 +0530

    04-09-2020

--- QA09_1.c
+++ QA09_1.c
@@ -1,10 +1,10 @@
-//
+// Find Square & Qube.
 // Created by KUNTAL OJHA RAJ on 9/4/2020.
-//
+// Square=n*n and Qube=n*n*n.
 #include <stdio.h>
 int main()
 {
-    int n,Square,Cube;
+    int n;
    scanf("%d",&n,printf("Enter a Number:\n"));
    printf("Square of This Number:%d\nCube Of This Number:%d",n*n,n*n*n);
     return 0;

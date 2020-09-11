365321191bcf66da21b67481a37d7aa074f2c620
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-09-12 00:09:20 +0530

    11-09-2020

---
+++ G_Started3.c
@@ -0,0 +1,12 @@
+// Exercise [E] (a)
+// Created by KUNTAL OJHA RAJ on 9/11/2020.
+// A Simple Program.
+#include <stdio.h>
+int main()
+{
+    int a;float b;int c;
+    a=25;b=3.24;c=a+b*b-35;
+    printf("A=%d\nB=%f\nC=%d",a,b,c);
+    return 0;
+}
+
---
+++ G_Started4.c
@@ -0,0 +1,9 @@
+// Exercise [E] (b)
+// Created by KUNTAL OJHA RAJ on 9/11/2020.
+// One more simple Example
+#include <stdio.h>
+int main()
+{
+    int a=35; float b=3.25;
+    printf("\n%d\n%f\n%d\n",a,b+1.5,235);
+}

83b65ec8c3901af5378a3c0d9b15103a09929c62
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-10-07 00:55:54 +0530

    07-10-2020

---
+++ C3_D_C_Instruction18_Q_C_b.c
@@ -0,0 +1,16 @@
+// [C] Attempt the following Question (b)
+// Created by KUNTAL OJHA RAJ on 10/6/2020.
+// Any integer is input through the keyboard. Write a program to find
+// the program to find out whether it is an odd number or ever number
+#include <stdio.h>
+int main()
+{
+    int i,j;
+    printf("Enter any number:\n");
+    scanf("%d",&i);
+    j=i;
+    if (i%2==0)
+        printf("The number is even:%d",j);
+    else
+        printf("The number is odd:%d",j);
+}

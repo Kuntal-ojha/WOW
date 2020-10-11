d986662f0fe66aeedaf489c87260efda193a1f15
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-10-11 20:45:24 +0530

    11-10-2020

--- Problem.c
+++ Problem.c
@@ -8,11 +8,13 @@
 #include <math.h>
 int main()
 {
-    int a,b=0;
+    int a,b=0,c=0;
     for (int i = 1; i > 0; ++i)
     {
         a=b*b;
+        c=pow(2,b);
         printf("My Name is KUNTAL OJHA %d\n",a);
+        printf("My Name is KUNTAL  %d\n\n",c);
         b=b+1;
     }
 }

ba0996437133242177bc6c750fcb52b121dbda1e
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-10-11 20:38:39 +0530

    11-10-2020

---
+++ Problem.c
@@ -0,0 +1,15 @@
+// Find X?
+// X^2-2^X
+// Created by KUNTAL OJHA RAJ on 10/11/2020.
+// Hear i Use pow() Function.
+// pow(3,2)=9 And i Use #include <math.h>
+// pow(a,b) = a^b
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    for (int i = 1; i > 0; ++i)
+    {
+        printf("My Name is KUNTAL OJHA\n");
+    }
+}

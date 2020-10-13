d0b20b82131b60402af1e213d9b9530aaf9d4ac5
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-10-13 11:19:57 +0530

    12-10-2020

---
+++ TCS_NQT2021_1.c
@@ -0,0 +1,19 @@
+// Q1) What is the output of the following Code.
+// Created by KUNTAL OJHA RAJ on 10/13/2020.
+// Very conceptual Questions
+#include <stdio.h>
+int main()
+{
+    while ()  // Its given Error Because While() no condition is hear.
+        printf("In Loop ");
+    printf("Out Loop");
+}
+/*
+ * a) In Loop
+ * b) Out Loop
+ * c) In Loop Out Loop
+ * d) INFINITE LOOP
+ * e) NONE OF THIS ABOVE
+ * f) ERROR
+ * Answer: f) ERROR.
+ */

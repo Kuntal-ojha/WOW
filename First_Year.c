ced418c56dac188384a593a49fc58ddbd73395ff
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-10-13 23:32:13 +0530

    13-10-2020

---
+++ First_Year.c
@@ -0,0 +1,9 @@
+// Print a *
+// Created by KUNTAL OJHA RAJ on 10/13/2020.
+// Very Basic program.
+#include <stdio.h>
+int main()
+{
+    printf("*");
+}
+
---
+++ First_Year_1.c
@@ -0,0 +1,10 @@
+// Display ** in Horizontal
+// Created by KUNTAL OJHA RAJ on 10/13/2020.
+// This is a vary basic program
+#include <stdio.h>
+int main()
+{
+    printf("**");
+    return 0;
+}
+

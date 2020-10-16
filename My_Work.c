bde82f62c107211b98941e2d0fb1bf93b7709726
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-10-16 22:33:38 +0530

    16-10-2020

---
+++ My_Work.c
@@ -0,0 +1,12 @@
+// Q3. Neso Academy
+// Created by KUNTAL OJHA RAJ on 10/16/2020.
+// What will be the Output of the following C Program Fragment:
+#include<stdio.h>
+int main()
+{
+    char c=255;
+    c=c+10;
+    printf("%d\n",c);
+
+    return 0;
+}

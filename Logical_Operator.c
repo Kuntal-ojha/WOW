cac94303db4a31f8c2da989514ff57f038c3c160
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-11-02 13:23:03 +0530

    2-11-2020

---
+++ Logical_Operator.c
@@ -0,0 +1,15 @@
+// Use Logical Operator. Not !
+// Created by KUNTAL OJHA RAJ on 11/2/2020.
+// Hear We Use Not ! Logical Operator.
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("Enter a Number:\n");
+    scanf("%d",&i);
+    if (!i)
+        printf("You Enter Zero.\n");
+    else
+        printf("You Enter Without Zero\n");
+    return 0;
+}

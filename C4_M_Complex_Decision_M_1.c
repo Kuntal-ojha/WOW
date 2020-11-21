74b0495accba64721f5d29d5f4c93064067d552e
Kuntal Ojha <kuntalojha2021@gmail.com>
2020-11-22 00:15:40 +0530

    22-11-2020

---
+++ C4_M_Complex_Decision_M_1.c
@@ -0,0 +1,17 @@
+// Simple Question Page:60.
+// Created by KUNTAL OJHA RAJ on 11/21/2020.
+// A Company insures its drives in the following cases:
+/*
+ * 1.If the driver is married.
+ * 2.If the driver is unmarried,male & above 30 years of age.
+ * 3.If the driver is unmarried, female & above 25 years of age.
+ * ==>
+ * In all others case, the driver is not insured.If the marital status,
+ * sex and age of the driver are the inputs, Write a program to determine
+ * whether the driver should be insured or note.
+ */
+#include <stdio.h>
+int main()
+{
+
+}

244c61cbc697a847e3e4ecd7068fa7a5dc672a5a
Kuntal Ojha <kuntalojha2021@gmail.com>
2021-01-10 22:40:35 +0530

    10-01-2021

---
+++ Pattern_1_1.c
@@ -0,0 +1,22 @@
+/*
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+ */
+// Created by KUNTAL OJHA RAJ on 1/10/2021.
+// How to drown pattern.
+#include <stdio.h>
+int main()
+{
+    for (int i = 0; i <=5 ; ++i) {
+        for (int j = 0; j <i; ++j) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
---
+++ Pattern_2.c
@@ -0,0 +1,21 @@
+/*
+
+*****
+****
+***
+**
+*
+
+ */
+// Created by KUNTAL OJHA RAJ on 1/10/2021.
+// How to drown pattern.
+#include <stdio.h>
+int main()
+{
+    for (int i = 0; i <5 ; ++i) {
+        for (int j = i; j <5; ++j) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}

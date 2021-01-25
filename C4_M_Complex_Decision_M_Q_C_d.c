92040538d8d71376b5513fcb0046262f53345554
Kuntal Ojha <kuntalojha2021@gmail.com>
2021-01-26 01:10:51 +0530

    25-01-2021

---
+++ C4_M_Complex_Decision_M_Q_C_d.c
@@ -0,0 +1,13 @@
+// Question [C] Point out the errors, if any, in the following program:
+// Created by KUNTAL OJHA RAJ on 1/25/2021.
+#include <stdio.h>
+int main()
+{
+    int x=2;
+    if (x==2 && x!=0)
+        printf("Hello\n");
+    else
+        printf("Bye\n");
+    return 0;
+}
+
---
+++ C4_M_Complex_Decision_M_Q_C_e.c
@@ -0,0 +1,11 @@
+// Question [C] Point out the errors, if any, in the following program:
+// Created by KUNTAL OJHA RAJ on 1/25/2021.
+// Good Question.
+#include <stdio.h>
+int main()
+{
+    int j=65;
+    printf("j>=65?%d:%c\n",j);
+    return 0;
+}
+

a11e9fe41aef43ceb0212fa494b59bb14a2ba6cc
Kuntal Ojha <kuntalojha2021@gmail.com>
2021-04-18 21:03:45 +0530

    18-04-2021

---
+++ Bitwise_Operator_XOR.c
@@ -0,0 +1,18 @@
+// Very Simple Example Bitwise XOR (^) Operator.
+// Bitwise Operator (^)
+// Created by KUNTAL OJHA RAJ on 4/18/2021.
+/**  AND (&) Operator Truth Table.
+ *      a  |  b | a^b
+ *-----------------------------
+ *      0  |  0 |  0
+ *      0  |  1 |  1
+ *      1  |  0 |  1
+ *      1  |  1 |  0
+ */
+#include <stdio.h>
+int main()
+{
+    int a=10,b=5;
+    printf("a^b=%d",a^b);
+    return 0;
+}
---
+++ Bitwise_Operator_XOR_1.c
@@ -0,0 +1,23 @@
+// Very Simple Example Bitwise OR (|) Operator .
+// Bitwise Operator (|)
+// Created by KUNTAL OJHA RAJ on 4/18/2021.
+/**  AND (&) Operator Truth Table.
+ *      a  |  b | a^b
+ *-----------------------------
+ *      0  |  0 |  0
+ *      0  |  1 |  1
+ *      1  |  0 |  1
+ *      1  |  1 |  0
+ */
+#include <stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter 1st number:\n");
+    scanf("%d",&a);
+    printf("Enter 2nd number:\n");
+    scanf("%d",&b);
+    c=a^b;
+    printf("Bitwise operation 1st and 2nd number is:\n%d",c);
+    return 0;
+}
